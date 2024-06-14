e009b1857a92d1f096b09e5bf8bc322598f683d6
amiralle <anmiralles@yahoo.es>
2024-06-14 17:23:08 +0200

    feat: adding comments to api

--- src/main/java/org/acme/getting/started/GreetingResource.java
+++ src/main/java/org/acme/getting/started/GreetingResource.java
@@ -12,6 +12,14 @@ public class GreetingResource {
     @Inject
     GreetingService service;
 
+    /**
+     * This method handles the HTTP GET request at the "/greeting/{name}" endpoint.
+     * It takes a single parameter, 'name', which is a string representing the name of the person to greet.
+     * The method returns a string that is the greeting message for the given name.
+     *
+     * @param name The name of the person to greet.
+     * @return A string containing the greeting message for the given name.
+     */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
